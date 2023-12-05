3f9f6bff939a280a7ee48d569e080ec10f2cf0df
Deldmon <127428932+Deldmon@users.noreply.github.com>
2023-12-05 22:28:33 +0200

    Aloitus

--- src/Tehtava.java
+++ src/Tehtava.java
@@ -3,8 +3,45 @@ public class Tehtava {
 
 	public static void main(String[] args) {
 		
-		System.out.println("-----");
-
+		Tietokone t1 = new Tietokone();
+		
+		t1.nayttoPaalle();
+		t1.setMuisti(32);
+		t1.tulostaTiedot();
 	}
 
+} // Mainin loppu 
+
+class Tietokone{
+	
+	String merkki = "Acer";
+	String malli = "FQZ71D";
+	private int muisti =0;
+	boolean paalla = true;
+	
+	public int getMuisti() {
+		return muisti;
+	}
+
+	public void setMuisti(int muisti) {
+		this.muisti = muisti;
+	}
+
+	public void nayttoPaalle() {
+		if(paalla == true)
+			System.out.println("Näyttö on päällä");
+	}
+	
+	public void tulostaTiedot() {
+		System.out.println("Merkki on: " + merkki);
+		System.out.println("Malli on: " + malli);
+		System.out.println("Muistin maara: " + muisti);
+
+		
+	}
+}
+
+class Naytto extends Tietokone{
+	
+	
 }
